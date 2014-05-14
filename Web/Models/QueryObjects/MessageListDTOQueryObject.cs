6424f446d77c2ecca254d3ca6b5c991ae877cbb8
E1ektr0 <E1ektr0.xyz@gmail.com>
2014-05-15 00:51:35 +0400

    Продолжение развития квери обджекта

--- Web/Models/QueryObjects/MessageListDTOQueryObject.cs
+++ Web/Models/QueryObjects/MessageListDTOQueryObject.cs
@@ -3,33 +3,49 @@ using System.Linq.Expressions;
 using DomainEntities;
 using Repositories.QueryObject;
 using Shared.Extensions;
+using Web.Controllers.Admin;
 using Web.Models.Dto;
 
 namespace Web.Models.QueryObjects
 {
+
+    public abstract class ModelQueryObject<TModel, TEntity> : QueryObject<TEntity> where TModel : new()
+    {
+        public void AddOrdering<TKeySelector>(Expression<Func<TModel, object>> keyExpression,
+            Expression<Func<TEntity, TKeySelector>> orderExpression)
+        {
+            AddOrdering(new TModel().GetPropertyName(keyExpression), orderExpression);
+        }
+    }
+
     /// <summary>
     /// Объект запроса для сообщений
     /// </summary>
-    public class MessageListDTOQueryObject : QueryObject<Message>
+    public class MessageListDTOQueryObject : ModelQueryObject<MessageListDTO,Message>
     {
         public MessageListDTOQueryObject()
         {
-            var messageListDTO = new MessageListDTO();
             //Связываем поля dto объекта с правилами сортировки
-            AddOrdering(messageListDTO.GetPropertyName(n => n.SendDate), n => n.SendDate);
-            AddOrdering(messageListDTO.GetPropertyName(n => n.ReceiverName), n => n.Receiver.FirstName);
-            AddOrdering(messageListDTO.GetPropertyName(n => n.SenderName), n => n.Sender.FirstName);
-            AddOrdering(messageListDTO.GetPropertyName(n => n.Subject), n => n.Subject);
+            AddOrdering(n => n.SendDate, n => n.SendDate);
+            AddOrdering(n => n.ReceiverName, n => n.Receiver.FirstName);
+            AddOrdering(n => n.SenderName, n => n.Sender.FirstName);
+            AddOrdering(n => n.Subject, n => n.Subject);
         }
     }
 
 
-    public class UsersQueryObject : QueryObject<MembershipUser>
+    public class UsersQueryObject : ModelQueryObject<UserModel, MembershipUser>
     {
         protected override Expression<Func<MembershipUser, bool>> Filter()
         {
             var filter = base.Filter();
-            //filter.And(n => n.Id == "");
+            if (!Search.IsNullOrEmpty())
+            {
+                filter.And(n => n.Id.Contains(Search));
+                filter.And(n => n.FirstName.Contains(Search));
+                filter.And(n => n.LastName.Contains(Search));
+            }
+
             return filter;
         }
     }
